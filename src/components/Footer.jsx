478bf589f102732e92572574dc096f0c02953a79
KaribelBT <karibel.barco@gmail.com>
2020-09-06 17:11:46 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,19 @@ class Footer extends Component {
         return (
             <ReactBootStrap.Container fluid className="footer fixed-bottom">
                 <ReactBootStrap.Row>
-                    <ReactBootStrap.Col className="d-flex align-items-center justify-content-center mb-2 mt-2 mb-lg-1 mt-lg-1">                       
-                        <ReactBootStrap.Button variant="secondary" className="btn btn-social mx-1"><i className="fab fa-github fa-2x"></i></ReactBootStrap.Button>
-                        <ReactBootStrap.Button variant="secondary" className="btn btn-social mx-1"><i className="fab fa-linkedin fa-2x"></i></ReactBootStrap.Button>
-                        <ReactBootStrap.Button variant="secondary" className="btn btn-social mx-1"><i className="fab fa-whatsapp fa-2x"></i></ReactBootStrap.Button>    
-                        <ReactBootStrap.Button variant="secondary" className="btn btn-social mx-1"><i className="far fa-envelope-open fa-2x"></i></ReactBootStrap.Button>  
+                    <ReactBootStrap.Col className="d-flex align-items-center justify-content-center mb-2 mt-2 mb-lg-1 mt-lg-1">
+                        <ReactBootStrap.Button variant="secondary" className="btn btn-social mx-1" target="_blank" rel="noopener noreferrer" href="https://github.com/KaribelBT">
+                            <i className="fab fa-github fa-2x"></i>
+                        </ReactBootStrap.Button>
+                        <ReactBootStrap.Button variant="secondary" className="btn btn-social mx-1" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/karibel-barco-taboada-8b62218a/">
+                            <i className="fab fa-linkedin fa-2x"></i>
+                        </ReactBootStrap.Button>
+                        <ReactBootStrap.Button variant="secondary" className="btn btn-social mx-1" target="_blank" rel="noopener noreferrer" href="https://wa.me/5491121685966/">
+                            <i className="fab fa-whatsapp fa-2x"></i>
+                        </ReactBootStrap.Button>
+                        <ReactBootStrap.Button variant="secondary" className="btn btn-social mx-1" target="_blank" rel="noopener noreferrer" href="mailto:karibel.barco@gmail.com">
+                            <i className="far fa-envelope-open fa-2x"></i>
+                        </ReactBootStrap.Button>
                     </ReactBootStrap.Col>
                 </ReactBootStrap.Row>
             </ReactBootStrap.Container>
